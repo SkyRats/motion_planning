5bfdd7c9840f836b6ee7aa3ca033c4a6f58a5cfc
fuoco <pedrofuoco6@gmail.com>
2020-09-11 09:00:42 -0300

    obstacle dilation applied to the map

--- scripts/map_mission.py
+++ scripts/map_mission.py
@@ -4,10 +4,12 @@ import ros_numpy
 from MAV import MAV
 from nav_msgs.msg import OccupancyGrid
 from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import LaserScan
 import math
 
 map_data = OccupancyGrid()
 pose_data = PoseStamped()
+laser_data = LaserScan()
 markers = {}
 
 def map_callback(data):
@@ -18,12 +20,16 @@ def pose_callback(data):
     global pose_data
     pose_data = data
 
+def laser_callback(data):
+    global laser_data
+    laser_data = data
+
 def enqueue(queue, obj):
     queue.append(obj)
     return obj
 
 def dequeue (queue):
-    return queue.pop()
+    return queue.pop(0)
 
 def mark(position,string):
     global markers
@@ -37,8 +43,9 @@ def map_pose(x,y):
     origin_x = map_data.info.origin.position.x
     origin_y = map_data.info.origin.position.y
     
-    row = (x - origin_x)/res
-    column = (y - origin_y)/res
+    row = (y - origin_y)/res
+    column = (x - origin_x)/res
+
 
     map_pose = int((row * width) + 1 + column)
     return map_pose 
@@ -62,6 +69,14 @@ def is_frontier(position):
         return False
     for n in adj(position):
         if n == -1:
+            return True
+    return False
+
+def is_obstacle_frontier(position):
+    if map_data.data[position] != 0 or map_data.data[position] == map_pose(pose_data.pose.position.x,pose_data.pose.position.y):
+        return False
+    for n in adj(position):
+        if n != -1 and n != 0:
             return True
     return False
 
@@ -102,29 +117,60 @@ def adj_pose(position):
 
 def checkpoint_selection(checkpoints):
     distances = {}
+    if checkpoints == []:
+        return []
     for point in checkpoints:
         distances[point] = distance(pose_data.pose.position.x, pose_data.pose.position.y,cartesian_pose(point)[0],cartesian_pose(point)[1])
     distances = sorted(distances.items(), key=lambda x:x[1], reverse=False)
-    if distances[0][1] < 0.2:
-        return distances[1][0]
     return distances[0][0]
 
-def WFD(initial):
+def WFD():
+    print("Started :)")
     Frontiers = []
     for i in range(map_data.info.width*map_data.info.height + 1):
-        markers[i-1] = "base"
-    
+        markers[i] = "base"
+    r = 0.7 #####RAIO DO DRONE#######
+    n = int(r/map_data.info.resolution)
+    queuem = []
+    initial = map_pose(pose_data.pose.position.x, pose_data.pose.position.y)
+    enqueue(queuem, initial)
+    camefrom = {}
+    while(len(queuem) != 0):
+        
+        p = dequeue(queuem)
+        if markers[p] == "First Wave" or type(markers[p]) == int:
+            continue
+        for adj in adj_pose(p):
+            if (map_data.data[adj] == 0):
+                if is_obstacle_frontier(adj):
+                    queued = []
+                    enqueue(queued, adj)
+                    markers[adj] = 1
+
+                    while (len(queued) != 0):
+                        p = dequeue(queued)
+                        if markers[p] >= n:
+                            continue
+                        for w in adj_pose(p):
+                            if type(markers[w]) != int and map_data.data[w] == 0:
+                                markers[w] = markers[p] + 1
+                                enqueue(queued, w)
+                            if is_obstacle_frontier(w) and (not markers[w] == 1):
+                                markers[w] = 1
+                                enqueue(queued, w)
+                if type(markers[adj]) != int and not markers[adj] == "First Wave":
+                    enqueue(queuem, adj)
+            markers[p] = "First Wave"
+    #########################################################################
     queuem = []
     queuef = []
     NewFrontier = []
     enqueue(queuem, initial)
     mark(initial, "Map-Open-List")
-    camefrom = {}
-
-
+    print("First wave completed")
     while(len(queuem) != 0):
         p = dequeue(queuem)
-        if markers[p] == "Map-Close-List":
+        if markers[p] == "Map-Close-List" or type(markers[p]) == int:
             continue
         if is_frontier(p):
             queuef = []
@@ -135,7 +181,7 @@ def WFD(initial):
             while len(queuef) != 0:
 
                 q = dequeue(queuef)
-                if markers[q] == "Map-Close-List" or markers[q] == "Frontier-Close-List":
+                if markers[q] == "Map-Close-List" or markers[q] == "Frontier-Close-List" or type(markers[q]) == int:
                     continue
                 if is_frontier(q):
                     NewFrontier.append(q)
@@ -151,12 +197,11 @@ def WFD(initial):
                 markers[point] = "Map-Close-List"
         for adj in adj_pose(p):
             if (map_data.data[adj] == 0):
-                if (markers[adj] != "Map-Open-List" and markers[adj] != "Map-Close-List"):
+                if (markers[adj] != "Map-Open-List" and markers[adj] != "Map-Close-List") and type(markers[adj]) != int:
                     enqueue(queuem, adj)
                     camefrom[adj] = p
                     markers[adj] = "Map-Open-List"
         markers[p] = "Map-Close-List"
-    
     return Frontiers, camefrom
 
 
@@ -164,52 +209,48 @@ def WFD(initial):
 def reconstruct_path(camefrom, current):
     total_path = []
     total_path.append (current)
-    print(camefrom[current])
     while camefrom.has_key(current):
         current = camefrom[current]
         total_path.append(current)
-    print(total_path)
     return total_path
-    
+
 
 def run():
     rospy.init_node("mapping")
     mav = MAV("1")
     initial_height = 1
     mav.takeoff(initial_height)
-    MASK_VELOCITY = 0b0000011111111000
+    MASK_POSITION = 0b0000011111111000
     map_sub = rospy.Subscriber("/map", OccupancyGrid, map_callback, queue_size=1)
     pose_sub = rospy.Subscriber("/slam_out_pose", PoseStamped, pose_callback, queue_size=1)
+    laser_sub = rospy.Subscriber("/laser/scan", LaserScan, laser_callback, queue_size=1)
     rospy.wait_for_message("/map", OccupancyGrid)
-    initial = map_pose(pose_data.pose.position.x, pose_data.pose.position.y)
+    rospy.wait_for_message("/slam_out_pose", PoseStamped)
     
 
-    while not rospy.is_shutdown():
+    while not rospy.is_shutdown(): 
         frontiers = []
         checkpoints = []
-        trajectory = WFD(initial)
+        trajectory = WFD()
         frontiers = trajectory[0]
         for frontier in frontiers:
             if len(frontier) >= 20:
                 frontier = sorted(frontier)
                 checkpoints.append(frontier[int(len(frontier)/2)])
         checkpoint = checkpoint_selection(checkpoints)
-        print(checkpoint)
-        if checkpoint == None:
-            print("mapping completed")
-            break
+        if checkpoint == []:
+            print("trying again")
+            continue
         path = reconstruct_path(trajectory[1], checkpoint)
-        print(path)
         path.reverse()
         print(path)
         for point in path:
-            while distance(pose_data.pose.position.x, pose_data.pose.position.y, cartesian_pose(point)[0], cartesian_pose(point)[1]) > 0.05:
-                mav.set_position_target(type_mask=MASK_VELOCITY,
+            while distance(pose_data.pose.position.x, pose_data.pose.position.y, cartesian_pose(point)[0], cartesian_pose(point)[1]) > 0.4:
+                mav.set_position_target(type_mask=MASK_POSITION,
                                     x_position=cartesian_pose(point)[0],
                                     y_position=cartesian_pose(point)[1],
                                     z_position=initial_height,
                                     yaw_rate= -pose_data.pose.orientation.z)
-                print(distance(pose_data.pose.position.x, pose_data.pose.position.y, cartesian_pose(point)[0], cartesian_pose(point)[1]))
     mav.land()
 
 if __name__ == '__main__':
