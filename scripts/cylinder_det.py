09286082343783f36398ef5f2eb5c496d4b8bac0
tomaz-suller <tomaz.suller@usp.br>
2021-01-24 14:02:28 -0300

    Add improvements from testing
    - Change circle property estimation method
    - Separate segmentation from classification
    - Return cylinder centre and radius in Cylinder object for ease of use

--- scripts/cylinder_det.py
+++ scripts/cylinder_det.py
@@ -3,19 +3,19 @@ import numpy as np
 import ros_numpy
 from sensor_msgs.msg import LaserScan
 
+from collections import namedtuple
+Cylinder = namedtuple('Cylinder', 'cx cy r')
+
 laser_data = LaserScan()
 
 CYLINDER_RADIUS = 0.25 #[m] - Raio dos cilindros (Especificado no edital)
 MINIMUM_DISTANCE = 0.15 #[m] - Distancia minima permitida pro Drone chegar perto de algo
-DISTANCE_THRESH = 0.05
-LENGTH_THRESH = 0.05
+DISTANCE_VARIATION_THRESH = 0.1
+RADIUS_VARIATION_THRESH = 0.06
 
 def laser_callback(data):
     global laser_data
     laser_data = data
-    cylinders, indices = detect_cylinders(laser_data)
-    print(len(cylinders))
-    print()
 
 def clear_cylinder(dist, index):
     temp = []
@@ -24,24 +24,21 @@ def clear_cylinder(dist, index):
     indices_temp.append(index)
     return temp, indices_temp
 
-def detect_cylinders(laser_data):
-    global MINIMUM_DISTANCE
-    global CYLINDER_RADIUS
-
+def get_cylinder_candidates(sensor):
     # Parametros de deteccao
     prop_max = (1/np.pi) * np.arccos((CYLINDER_RADIUS + MINIMUM_DISTANCE) / (np.sqrt(2*CYLINDER_RADIUS**2 + 2*CYLINDER_RADIUS*MINIMUM_DISTANCE + MINIMUM_DISTANCE**2)))
+    res_min = 3 
     var_max = np.sqrt(2*CYLINDER_RADIUS**2 + 2*CYLINDER_RADIUS*MINIMUM_DISTANCE + MINIMUM_DISTANCE**2) - MINIMUM_DISTANCE
 
-    objetos = []    # A lista de objetos que v o ser devolvidas
-    sensor = np.array(laser_data.ranges)
     res = len(sensor)
     sensor = np.where(sensor == np.Inf, 0, sensor)
 
     dists = sensor[sensor > 0]    # Lista ignorando os zeros
     indices = [ i for i in range(res) if sensor[i] > 0 ] # Correspondencia entre indices do array sem zeros e do scan do lidar
 
-    temp = []   # Lista temporaria pra guardar os objetos que v o ser inseridos em "objetos"
+    objetos = []    # A lista de objetos que v o ser devolvidas
     obj_indices = []    # Lista dos indices da lista "dists" que representam os cilindros
+    temp = []   # Lista temporaria pra guardar os objetos que v o ser inseridos em "objetos"
     indices_temp = []   # Armazena os indices que vao ser colocados em "obj_indices"
 
     if len(dists) > 0:
@@ -61,7 +58,7 @@ def detect_cylinders(laser_data):
             # Nesse caso, seria chao, entao reinicializa as listas temporarias
             else:
 
-                if 0 < len(temp) < prop_max*res and object_is_a_circle(temp, indices_temp):   # Se nenhum dos de cima, entao   um cilindro. Ai o codigo bota nas listas "objetos" e "obj_indices" e reinicializa os temporarios
+                if res_min < len(temp) < prop_max*res:   # Se nenhum dos de cima, entao   um cilindro. Ai o codigo bota nas listas "objetos" e "obj_indices" e reinicializa os temporarios
                     objetos.append(temp)
                     obj_indices.append( indices_temp )
 
@@ -85,7 +82,7 @@ def detect_cylinders(laser_data):
                 previous_i = i % len(dists)
                 current_i = (i-1) % len(dists)
 
-            if (0 < len(temp) < prop_max*res or np.average(temp) < 0.7) and object_is_a_circle(temp, indices_temp):
+            if (res_min < len(temp) < prop_max*res or np.average(temp) < 0.7):
                 objetos.append(temp)
                 obj_indices.append( indices_temp )
 
@@ -97,108 +94,111 @@ def detect_cylinders(laser_data):
             Assim, so precisamos conferir se o objeto e tao grande que ele nem foi detectado na primeira passagem
             """
 
-            if obj_indices and obj_indices[0][0] < indices[current_i] < obj_indices[0][1]:
+            if len(obj_indices[0]) > 1 or obj_indices[0][0] < indices[current_i] < obj_indices[0][1]:
                 del objetos[0]
                 del obj_indices[0]
 
         # Se chega so final da lista com um objeto valido armazenado, salva ele na lista
-        elif 0 < len(temp) < prop_max*res and object_is_a_circle(temp, indices_temp):
+        elif res_min < len(temp) < prop_max*res:
             obj_indices.append(indices_temp)
             objetos.append(temp)
-
+    
     return objetos, obj_indices
 
-def object_is_a_circle(obj, obj_indices):
+def get_cylinders_xy_and_radius(obj, obj_indices):
+    cylinders = []
+    for i in range(len(obj)):
+        cx, cy, r = find_circle_properties(obj[i], obj_indices[i])
+        if radii_are_similar(CYLINDER_RADIUS, r):
+            cylinders.append( Cylinder(cx, cy, r) )
+    return cylinders
+
+def find_circle_properties(obj, obj_indices):
     cx_average = cy_average = r_average = 0
 
     if len(obj) > 3:
-        thetas = np.multiply(obj_indices, laser_data.angle_increment)
-        - laser_data.angle_min
+        thetas = indices_to_angles(obj_indices)
 
-        for i in range(len(obj)-3):
-            x1, y1 = polar_to_cartesian(obj[i], thetas[i])
-            x2, y2 = polar_to_cartesian(obj[i+1], thetas[i+1])
-            x3, y3 = polar_to_cartesian(obj[i+2], thetas[i+2])
+        final_index = len(obj)-1 
+        x1, y1 = polar_to_cartesian(obj[0], thetas[0])
+        x2, y2 = polar_to_cartesian(obj[final_index], thetas[final_index])
 
+        average_index = 1
+        centre = len(obj)//2
+        distance_from_centre = 0
+        i = centre
+        is_first_pass = True
+        while 0 < i < final_index:
+            if i < centre:
+                i = centre + distance_from_centre
+                distance_from_centre += 1
+            else:
+                i = centre - distance_from_centre
+            
+            x3, y3 = polar_to_cartesian(obj[i], thetas[i])
             cx, cy, r = find_circle_xy_and_radius(x1, y1, x2, y2, x3, y3)
-            if i == 0:
+
+            if is_first_pass:
                 cx_average = cx
                 cy_average = cy
                 r_average = r
+                is_first_pass = False
+                distance_from_centre += 1
                 continue
 
             if (centres_are_close(cx, cy, cx_average, cy_average)
                 and radii_are_similar(r, r_average)):
-                cx_average = (cx_average*i + cx) / (i+1)
-                cy_average = (cy_average*i + cx) / (i+1)
-                r_average = (r_average*i + cx) / (i+1)
-                pass
-            else:
-                return False
+                cx_average = (cx_average*average_index + cx) / (average_index+1)
+                cy_average = (cy_average*average_index + cy) / (average_index+1)
+                r_average = (r_average*average_index + r) / (average_index+1)
+                average_index += 1
 
-        print(cx_average, cy_average, r_average)
-        return True
-
-    return False
+    return cx_average, cy_average, r_average
 
 def find_circle_xy_and_radius(x1, y1, x2, y2, x3, y3):
-    # From
-    # https://www.geeksforgeeks.org/equation-of-circle-when-three-points-on-the-circle-are-given/
-    x12 = x1 - x2
-    x13 = x1 - x3
+    # Source
+    # https://www.xarg.org/2018/02/create-a-circle-out-of-three-points/
+    a = x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2
 
-    y12 = y1 - y2
-    y13 = y1 - y3
+    b = ( (x1 * x1 + y1 * y1) * (y3 - y2)
+        + (x2 * x2 + y2 * y2) * (y1 - y3)
+        + (x3 * x3 + y3 * y3) * (y2 - y1) )
+ 
+    c = ( (x1 * x1 + y1 * y1) * (x2 - x3) 
+        + (x2 * x2 + y2 * y2) * (x3 - x1) 
+        + (x3 * x3 + y3 * y3) * (x1 - x2) )
+ 
+    cx = -b / (2 * a)
+    cy = -c / (2 * a)
 
-    y31 = y3 - y1
-    y21 = y2 - y1
+    r = np.sqrt( (cx-x1)**2 + (cy-y1)**2 )
 
-    x31 = x3 - x1
-    x21 = x2 - x1
-
-    sx13 = pow(x1, 2) - pow(x3, 2)
-    sy13 = pow(y1, 2) - pow(y3, 2)
-
-    sx21 = pow(x2, 2) - pow(x1, 2)
-    sy21 = pow(y2, 2) - pow(y1, 2)
-
-    f = (((sx13) * (x12) + (sy13) *
-        (x12) + (sx21) * (x13) +
-        (sy21) * (x13)) // (2 *
-        ((y31) * (x12) - (y21) * (x13))))
-
-    g = (((sx13) * (y12) + (sy13) * (y12) +
-        (sx21) * (y13) + (sy21) * (y13)) //
-        (2 * ((x31) * (y12) - (x21) * (y13))))
-
-    c = (-pow(x1, 2) - pow(y1, 2) -
-        2 * g * x1 - 2 * f * y1)
-
-    # eqn of circle be x^2 + y^2 + 2*g*x + 2*f*y + c = 0
-    # where centre is (h = -g, k = -f) and
-    # radius r as r^2 = h^2 + k^2 - c
-    h = -g
-    k = -f
-    square_of_r = h * h + k * k - c
-
-    # r is the radius
-    r = np.sqrt(square_of_r)
-
-    return h, k, r
+    return cx, cy, r
 
 def polar_to_cartesian(r, theta):
     x = r*np.cos(theta)
     y = r*np.sin(theta)
     return x, y
 
+def indices_to_angles(obj_indices):
+    return np.radians(np.array(obj_indices) - 90)
+
 def centres_are_close(x1, y1, x2, y2):
-    return True if np.linalg.norm([x1-x2, y1-y2]) < DISTANCE_THRESH else False
+    return True if np.linalg.norm([x1-x2, y1-y2]) < DISTANCE_VARIATION_THRESH else False
 
 def radii_are_similar(r1, r2):
-    return True if abs(r1-r2) < LENGTH_THRESH else False
+    return True if abs(r1-r2) < RADIUS_VARIATION_THRESH else False
 
 if __name__ == '__main__':
     rospy.init_node("avoidance")
     laser_sub = rospy.Subscriber("/laser/scan", LaserScan,
         laser_callback, queue_size=1)
-    rospy.spin()
+    rate = rospy.Rate(20)
+    while not rospy.is_shutdown():
+        rospy.wait_for_message('/laser/scan', LaserScan)
+        cylinders = get_cylinders_xy_and_radius(laser_data)
+        print(len(cylinders))
+        for cylinder in cylinders:
+            print("{0}\t{1}\t\t{2}".format(cylinder.cx, cylinder.cy, cylinder.r))
+        print("\n")
+        rate.sleep()
