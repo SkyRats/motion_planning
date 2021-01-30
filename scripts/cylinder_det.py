13f77551400021bb0a8e2b96f90cf4d05540e514
pedro-fuoco <pedrofuoco6@gmail.com>
2021-01-29 22:31:39 -0300

    rrt working without mav

---
+++ scripts/cylinder_det.py
@@ -0,0 +1,209 @@
+import rospy
+import numpy as np
+import ros_numpy
+from sensor_msgs.msg import LaserScan
+
+from collections import namedtuple
+Cylinder = namedtuple('Cylinder', 'cx cy r')
+
+CYLINDER_RADIUS = 0.25 #[m] - Raio dos cilindros (Especificado no edital)
+MINIMUM_DISTANCE = 0.15 #[m] - Distancia minima permitida pro Drone chegar perto de algo
+DISTANCE_VARIATION_THRESH = 0.1
+RADIUS_VARIATION_THRESH = 0.06
+
+class cylinder_detector():
+
+    def clear_cylinder(self, dist, index):
+        temp = []
+        indices_temp = []
+        temp.append(dist)
+        indices_temp.append(index)
+        return temp, indices_temp
+
+    def get_cylinder_candidates(self, sensor):
+        # Parametros de deteccao
+        prop_max = (1/np.pi) * np.arccos((CYLINDER_RADIUS + MINIMUM_DISTANCE) / (np.sqrt(2*CYLINDER_RADIUS**2 + 2*CYLINDER_RADIUS*MINIMUM_DISTANCE + MINIMUM_DISTANCE**2)))
+        res_min = 3 
+        var_max = np.sqrt(2*CYLINDER_RADIUS**2 + 2*CYLINDER_RADIUS*MINIMUM_DISTANCE + MINIMUM_DISTANCE**2) - MINIMUM_DISTANCE
+
+        res = len(sensor)
+        sensor = np.where(sensor == np.Inf, 0, sensor)
+
+        dists = sensor[sensor > 0]    # Lista ignorando os zeros
+        indices = [ i for i in range(res) if sensor[i] > 0 ] # Correspondencia entre indices do array sem zeros e do scan do lidar
+
+        objetos = []    # A lista de objetos que v o ser devolvidas
+        obj_indices = []    # Lista dos indices da lista "dists" que representam os cilindros
+        temp = []   # Lista temporaria pra guardar os objetos que v o ser inseridos em "objetos"
+        indices_temp = []   # Armazena os indices que vao ser colocados em "obj_indices"
+
+        if len(dists) > 0:
+
+            temp.append( dists[0] )
+            indices_temp.append( indices[0] )
+            i = 1   # Comeca de 1 porque analisa os elementos i-1 e i no loop
+            # Passa por todos os elementos nao nulos do scan
+            while i < len(dists):
+
+                # V  se o pr ximo elemento da lista tem uma variacao "permitida" de acordo com a f rmula
+                if abs(dists[i] - dists[i-1]) <= var_max and abs(indices[i] - indices[i-1]) == 1:
+                    temp.append( dists[i] )
+                    indices_temp.append( indices[i] )
+
+                # Se o proximo elemento nao for mais um objeto de acordo com var_max, o codigo analisa o tamanho do objeto e faz a propor o pra ver se pode ser um cilindro
+                # Nesse caso, seria chao, entao reinicializa as listas temporarias
+                else:
+
+                    if res_min < len(temp) < prop_max*res:   # Se nenhum dos de cima, entao   um cilindro. Ai o codigo bota nas listas "objetos" e "obj_indices" e reinicializa os temporarios
+                        objetos.append(temp)
+                        obj_indices.append( indices_temp )
+
+                    (temp, indices_temp) = self.clear_cylinder(dists[i], indices[i])
+
+                i += 1
+
+            # Confere se existe um elemento que passa pela "quebra" do lidar
+            # Se existe um elemento nao nulo no final do scan e se o primeiro elemento nao nulo tem indice 0
+            if sensor[-1] != 0 and indices[0] == 0:
+
+                # Roda pela lista de objetos nao nulos de novo, ate que a distancia entre eles indique que o objeto acabou
+                previous_i = i % len(dists)
+                current_i = (i-1) % len(dists)
+
+                while abs(dists[previous_i ] - dists[ current_i ]) <= var_max and i < 2*len(dists) and abs(indices[previous_i] - indices[current_i]) == 1:
+                    temp.append(dists[previous_i ])
+                    indices_temp.append(indices[previous_i])
+
+                    i += 1
+                    previous_i = i % len(dists)
+                    current_i = (i-1) % len(dists)
+
+                if (res_min < len(temp) < prop_max*res or np.average(temp) < 0.7):
+                    objetos.append(temp)
+                    obj_indices.append( indices_temp )
+
+                """
+                Se o objeto na fronteira for valido, entao esse objeto foi quebrado em 2, sendo capturado pela primeira passagem,
+                entao o objeto 0 deve ser apagado
+                Se o objeto nao for valido, ou seja, se o objeto for grande demais, ele foi quebrado em 2 partes, sendo uma delas detectada pela primeira passagem,
+                    entao o objeto 0 deve ser apagado
+                Assim, so precisamos conferir se o objeto e tao grande que ele nem foi detectado na primeira passagem
+                """
+
+                if len(obj_indices[0]) > 1 or obj_indices[0][0] < indices[current_i] < obj_indices[0][1]:
+                    del objetos[0]
+                    del obj_indices[0]
+
+            # Se chega so final da lista com um objeto valido armazenado, salva ele na lista
+            elif res_min < len(temp) < prop_max*res:
+                obj_indices.append(indices_temp)
+                objetos.append(temp)
+        
+        return objetos, obj_indices
+
+    def get_cylinders_xy_and_radius(self, sensor):
+        objects, objects_indices = self.get_cylinder_candidates(sensor)
+        cylinders = []
+        for i in range(len(objects)):
+            cx, cy, r = self.find_circle_properties(objects[i], objects_indices[i])
+            if self.radii_are_similar(CYLINDER_RADIUS, r):
+                cylinders.append( Cylinder(cx, cy, r) )
+        return cylinders
+
+    def find_circle_properties(self, obj, obj_indices):
+        cx_average = cy_average = r_average = 0
+
+        if len(obj) > 3:
+            thetas = self.indices_to_angles(obj_indices)
+
+            final_index = len(obj)-1 
+            x1, y1 = self.polar_to_cartesian(obj[0], thetas[0])
+            x2, y2 = self.polar_to_cartesian(obj[final_index], thetas[final_index])
+
+            average_index = 1
+            centre = len(obj)//2
+            distance_from_centre = 0
+            i = centre
+            is_first_pass = True
+            while 0 < i < final_index:
+                if i < centre:
+                    i = centre + distance_from_centre
+                    distance_from_centre += 1
+                else:
+                    i = centre - distance_from_centre
+                
+                x3, y3 = self.polar_to_cartesian(obj[i], thetas[i])
+                cx, cy, r = self.find_circle_xy_and_radius(x1, y1, x2, y2, x3, y3)
+
+                if is_first_pass:
+                    cx_average = cx
+                    cy_average = cy
+                    r_average = r
+                    is_first_pass = False
+                    distance_from_centre += 1
+                    continue
+
+                if (self.centres_are_close(cx, cy, cx_average, cy_average)
+                    and self.radii_are_similar(r, r_average)):
+                    cx_average = (cx_average*average_index + cx) / (average_index+1)
+                    cy_average = (cy_average*average_index + cy) / (average_index+1)
+                    r_average = (r_average*average_index + r) / (average_index+1)
+                    average_index += 1
+
+        return cx_average, cy_average, r_average
+
+    def find_circle_xy_and_radius(self, x1, y1, x2, y2, x3, y3):
+        # Source
+        # https://www.xarg.org/2018/02/create-a-circle-out-of-three-points/
+        a = x1 * (y2 - y3) - y1 * (x2 - x3) + x2 * y3 - x3 * y2
+
+        b = ( (x1 * x1 + y1 * y1) * (y3 - y2)
+            + (x2 * x2 + y2 * y2) * (y1 - y3)
+            + (x3 * x3 + y3 * y3) * (y2 - y1) )
+    
+        c = ( (x1 * x1 + y1 * y1) * (x2 - x3) 
+            + (x2 * x2 + y2 * y2) * (x3 - x1) 
+            + (x3 * x3 + y3 * y3) * (x1 - x2) )
+    
+        cx = -b / (2 * a)
+        cy = -c / (2 * a)
+
+        r = np.sqrt( (cx-x1)**2 + (cy-y1)**2 )
+
+        return cx, cy, r
+
+    def polar_to_cartesian(self, r, theta):
+        y = -r*np.cos(theta)
+        x = r*np.sin(theta)
+        return x, y
+
+    def indices_to_angles(self, obj_indices):
+        return np.radians(np.array(obj_indices) - 90)
+
+    def centres_are_close(self, x1, y1, x2, y2):
+        return True if np.linalg.norm([x1-x2, y1-y2]) < DISTANCE_VARIATION_THRESH else False
+
+    def radii_are_similar(self, r1, r2):
+        return True if abs(r1-r2) < RADIUS_VARIATION_THRESH else False
+
+
+laser_data = LaserScan()
+def laser_callback(data):
+    global laser_data
+    laser_data = data
+
+if __name__ == '__main__':
+    rospy.init_node("avoidance")
+    laser_sub = rospy.Subscriber("/laser/scan", LaserScan,
+        laser_callback, queue_size=1)
+    rate = rospy.Rate(20)
+
+    cyl = cylinder_detector()
+    while not rospy.is_shutdown():
+        rospy.wait_for_message('/laser/scan', LaserScan)
+        cylinders = cyl.get_cylinders_xy_and_radius(laser_data.ranges)
+        print(len(cylinders))
+        for cylinder in cylinders:
+            print("{0}\t{1}\t\t{2}".format(cylinder.cx, cylinder.cy, cylinder.r))
+        print("\n")
+        rate.sleep()
---
+++ scripts/rrt-fnd.py
@@ -0,0 +1,240 @@
+import sys, random, math, pygame, time
+from pygame.locals import *
+from math import sqrt, cos, sin, atan2
+import numpy as np
+from cylinder_det import cylinder_detector
+import rospy
+from sensor_msgs.msg import LaserScan
+from scipy.ndimage import morphology
+
+class node:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.cost = 0.0
+        self.parent = None
+        self.children = []
+    
+class dinamic_obstacle_avoidance:
+    def __init__(self, goal_x):
+        #PYGAME
+        self.XDIM = 120
+        self.YDIM = 180
+        self.WINSIZE = [self.XDIM, self.YDIM]
+
+        self.screen = pygame.display.set_mode(self.WINSIZE)
+        black = 20, 20, 40
+        self.screen.fill(black)
+        pygame.display.update()
+
+
+        #ROS
+        self.laser_sub = rospy.Subscriber("/laser/scan", LaserScan, self.laser_callback, queue_size=1)
+        rospy.wait_for_message('/laser/scan', LaserScan)
+        
+
+        #RRT PARAMS
+        self.goal_x = goal_x
+        self.EPSILON = 5
+        self.rrt_near_radius = 15
+        self.current_solution = []
+        self.nodeList = []
+
+        #Dinamic Map
+        self.cyl = cylinder_detector()
+        self.update_map()
+
+    def laser_callback(self,data):
+        self.laser_data = data
+    
+    def distance(self,x1,y1,x2,y2): #JA ESTA NO MOTION
+        return math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))
+
+    def nearest(self,point): #REQUER OTIMIZACAO
+        nearestNode = self.start
+        for node in self.nodeList:
+            if self.distance(node.x,node.y,point[0],point[1]) < self.distance(nearestNode.x,nearestNode.y,point[0],point[1]):
+                nearestNode = node
+        return nearestNode
+
+    def steer(self,origin,stretch):
+        if self.distance(origin.x,origin.y,stretch[0],stretch[1]) < self.EPSILON:
+            return stretch
+        theta = atan2(stretch[1] - origin.y, stretch[0] - origin.x)
+        return [int(origin.x + self.EPSILON * cos(theta)),int(origin.y + self.EPSILON * sin(theta))]                
+    
+    def collision(self,node,map):
+        if map[node.x-1][node.y-1] == 100:
+            return True
+        else:
+            return False
+    
+    def near(self,newNode,near_radius):
+        nearList = []
+        for node in self.nodeList:
+            if self.distance(node.x,node.y,newNode.x,newNode.y) < near_radius:
+                nearList.append(node)
+        return nearList
+    
+    def chooseparent(self,newNode,nearList):
+        nodeMin = nearList[0]
+        cmin = nodeMin.cost + self.distance(nodeMin.x,nodeMin.y,newNode.x,newNode.y)
+        for node in nearList:
+            if node.cost + self.distance(node.x,node.y,newNode.x,newNode.y) < cmin:
+                cmin = node.cost + self.distance(node.x,node.y,newNode.x,newNode.y)
+                nodeMin = node
+        
+        newNode.parent = nodeMin
+        newNode.cost = cmin
+        nodeMin.children.append(newNode)
+    
+    def rewire(self,nearList,newNode):
+        for node in nearList:
+            if newNode.cost + self.distance(newNode.x,newNode.y,node.x,node.y) < node.cost:
+                node.parent.children.remove(node)
+                pygame.draw.line(self.screen, (20, 20, 40), (node.x,node.y),(node.parent.x,node.parent.y))
+                node.parent = newNode
+                newNode.children.append(node)
+                pygame.draw.line(self.screen, (0, 240, 0), (node.x,node.y),(newNode.x,newNode.y))
+                node.cost = newNode.cost + self.distance(newNode.x,newNode.y,node.x,node.y)
+    
+    def forceRemoval(self): #requer otimizacao otimizacao (?)
+        childless = []
+        for node in self.nodeList:
+            if node in self.current_solution:
+                continue
+            if node.children == []:
+                childless.append(node)
+        removed = childless[random.randint(0,len(childless)-1)]
+        removed.parent.children.remove(removed)
+        pygame.draw.line(self.screen, (20, 20, 40), (removed.x,removed.y),(removed.parent.x,removed.parent.y))
+        self.nodeList.remove(removed)
+    
+    def generate_solution(self,node_goal):
+        solution = []
+        solution.append(node_goal)
+        while node_goal.parent != None:
+            solution.append(node_goal)
+            node_goal = node_goal.parent
+        solution.append(node_goal)
+        solution.reverse()
+        return solution
+
+    def rrt_star_fn(self, start, i=0):
+        self.start = start
+        self.nodeList.append(self.start)
+        t0 = time.time()
+        found = False
+        while i < 1500 or not found:
+            rndPoint = [int(random.random() * 120.0), int(random.random() * 180.0)]
+            
+            nearestNode = self.nearest(rndPoint)
+
+            newNode = self.steer(nearestNode,rndPoint)
+            newNode = node(newNode[0],newNode[1])
+
+            if not self.collision(newNode, self.DynamicMap):
+                nearList = self.near(newNode, self.rrt_near_radius)
+
+                self.chooseparent(newNode,nearList)
+
+                self.nodeList.append(newNode)
+                
+                self.rewire(nearList,newNode)
+
+                pygame.draw.line(self.screen, (0,240,0), (newNode.x,newNode.y),(newNode.parent.x,newNode.parent.y))
+                pygame.display.update()
+
+
+                if len(self.nodeList) > 2000:
+                    self.forceRemoval()
+            ####################################################
+                found = newNode.x >= self.goal_x
+                if found:
+                    node_goal = newNode
+            i += 1
+        t1 = time.time()
+        print(t1-t0)
+        return self.generate_solution(node_goal)
+    
+    def update_map(self):
+        self.DynamicMap = np.zeros((120,180))
+        dilate = 5
+        cylinders = self.cyl.get_cylinders_xy_and_radius(self.laser_data.ranges)
+        for i in range(len(cylinders)):
+            x = int(cylinders[i].cx/ 0.05)
+            y = int((cylinders[i].cy + 4.5)/ 0.05) #Correct later
+            obstacle = node(x,y)
+            for collied_node in self.near(obstacle,dilate):
+                if collied_node in self.nodeList:
+                    self.DeleteBranch(collied_node)
+            if x > 120 or y > 180:
+                continue
+            self.DynamicMap[x-1][y-1] = 100
+        self.DynamicMap = morphology.grey_dilation(self.DynamicMap, size=(dilate*2,dilate*2))
+
+        #import matplotlib.pyplot as plt
+        #plt.imshow(self.DynamicMap, cmap='hot', interpolation='nearest')
+        #plt.show()
+
+    def CollisionInPath(self,safety):
+        if safety > len(self.current_solution):
+            safety = len(self.current_solution)
+        for num in range(safety):
+            if self.current_solution[num] not in self.nodeList:
+                return True
+        return False
+    
+    def DeleteBranch(self,node,exeption = None):
+        child_list = []
+        child_list.append(node)
+        for child in node.children:
+            if child == exeption:
+                child.parent = None
+                continue
+            child_list.append(child)
+
+        for child in child_list:
+            for kids in child.children:
+                child_list.append(kids)
+        done = []
+        for child in child_list:
+            if child in done:
+                continue
+            self.nodeList.remove(child)
+            if child.parent != None:
+                pygame.draw.line(self.screen, (20,20,40), (child.x,child.y), (child.parent.x,child.parent.y))
+                child.parent.children.remove(child)
+            done.append(child)
+        
+    def nextNode(self):
+        last_node = self.current_solution.pop(0)
+        self.DeleteBranch(last_node, self.current_solution[0])
+        self.start = self.current_solution[0]
+        #drone goes to next node
+        time.sleep(0.75)
+        print("forward")
+
+
+    def dynamic_rrt(self):
+        self.current_solution = self.rrt_star_fn(node(0,90))
+        while self.current_solution[0].x < self.goal_x and not rospy.is_shutdown():
+            pygame.display.update()
+            if self.collision(self.current_solution[0],self.DynamicMap):
+                print("collided")
+                return
+            print(len(self.current_solution))
+            print(self.current_solution[0].x,self.current_solution[0].y)
+            self.update_map()
+            if self.CollisionInPath(60):
+                #StopMovement()
+                self.current_solution = self.rrt_star_fn(self.current_solution[0], 1500)
+                print("new path acquired")
+                continue
+            self.nextNode()
+        print("complete!")
+
+if __name__ == "__main__":
+    rospy.init_node("dynamic_avoidance")
+    doa = dinamic_obstacle_avoidance(110)
+    doa.dynamic_rrt()
